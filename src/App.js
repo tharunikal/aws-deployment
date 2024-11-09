5af281d40c1d07c9dd55d5f045ea2c8e70cfe617
Tharunika L <tharunika.l14@gmail.com>
2024-11-09 22:20:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>No Longer Human<-Dazai/h1>
+      <h1>No Longer Human-Dazai</h1>
       <p>To add your details, please submit a PR with your information in the <code>developers.json</code> file.</p>
       <CardList />
     </div>
